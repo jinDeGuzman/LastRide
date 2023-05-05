813f4a97aea7a74dbf67a0133a3b1265f0c0976f
Jin <jindebest@gmail.com>
2023-05-05 12:42:06 -0700

    Third screen to chose difficulty is made.

---
+++ app/src/main/java/com/example/lastride/difficulty.java
@@ -0,0 +1,13 @@
+package com.example.lastride;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class difficulty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difficulty);
+    }
+}
