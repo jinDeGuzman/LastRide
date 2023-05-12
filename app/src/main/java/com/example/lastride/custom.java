68bea37753cec82cfd3fa597b34edd4bdb7b1d01
91093935 <jinadeguzman14@gmail.com>
2023-05-12 12:22:07 -0700

    custom screen button made.

---
+++ app/src/main/java/com/example/lastride/custom.java
@@ -0,0 +1,16 @@
+package com.example.lastride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class custom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom);
+
+
+    }
+}
--- app/src/main/java/com/example/lastride/difficulty.java
+++ app/src/main/java/com/example/lastride/difficulty.java
@@ -1,7 +1,11 @@
 package com.example.lastride;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class difficulty extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class difficulty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_difficulty);
+        Button btn = findViewById(R.id.button5);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(difficulty.this, custom.class));
+            }
+        });
     }
 }
