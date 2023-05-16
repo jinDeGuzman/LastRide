9ab49087940dda8c7acc6a4bb59aae61f52dc5d0
Jin <jindebest@gmail.com>
2023-05-16 12:33:06 -0700

    all the images are updated

--- app/src/main/java/com/example/lastride/custom.java
+++ app/src/main/java/com/example/lastride/custom.java
@@ -2,7 +2,10 @@ package com.example.lastride;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class custom extends AppCompatActivity {
@@ -13,7 +16,14 @@ public class custom extends AppCompatActivity {
         setContentView(R.layout.activity_custom);
 
         EditText guess;
-        guess =(EditText)findViewById(R.id.EditView);
+        guess = (EditText) findViewById(R.id.EditView);
         String Answer = "hello";
+        Button btn = findViewById(R.id.Btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  startActivity(new Intent());
+            }
+        });
     }
-}
+}//whattt why does this work l
